9f1efc9611bc997e02cfd009b45726189ec6bc9f
Tạ Quang Khôi <khoi0941@gmail.com>
2023-09-05 17:35:42 +0700

    get content of markdown file

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Layout, {siteTitle} from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
-import { getSortedPostsData } from '../lib/posts';
+import {getSortedPostsData} from '../lib/posts';
 
 export async function getStaticProps() {
     const allPostsData = getSortedPostsData();
@@ -12,7 +12,7 @@ export async function getStaticProps() {
     };
 }
 
-export default function Home({ allPostsData }) {
+export default function Home({allPostsData}) {
     return (
         <Layout home>
             <Head>
@@ -32,12 +32,12 @@ export default function Home({ allPostsData }) {
             <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
                 <h2 className={utilStyles.headingLg}>Blog</h2>
                 <ul className={utilStyles.list}>
-                    {allPostsData.map(({ id, date, title }) => (
+                    {allPostsData.map(({id, date, title}) => (
                         <li className={utilStyles.listItem} key={id}>
                             {title}
-                            <br />
+                            <br/>
                             {id}
-                            <br />
+                            <br/>
                             {date}
                         </li>
                     ))}
