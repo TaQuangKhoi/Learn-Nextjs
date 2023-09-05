b8553685026212d870fc64a7ae8712f69a463e3f
Tạ Quang Khôi <khoi0941@gmail.com>
2023-09-05 16:32:24 +0700

    Done Layout

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -18,11 +18,6 @@ export default function FirstPost() {
                 }
             />
             <h1>First Post</h1>
-            <h2>
-                <Link href="/">
-                    <Button variant="contained">Back to home</Button>
-                </Link>
-            </h2>
         </Layout>
     );
 }
