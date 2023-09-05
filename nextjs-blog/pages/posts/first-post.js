1e5423dad6d261f95a399271bd94cd2757622926
Tạ Quang Khôi <khoi0941@gmail.com>
2023-09-05 15:57:45 +0700

    Use @mui/material/Button

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,11 +1,14 @@
 import Link from 'next/link';
+import Button from '@mui/material/Button';
 
 export default function FirstPost() {
     return (
         <>
             <h1>First Post</h1>
             <h2>
-                <Link href="/">Back to home</Link>
+                <Link href="/">
+                    <Button variant="contained">Back to home</Button>
+                </Link>
             </h2>
         </>
     );
